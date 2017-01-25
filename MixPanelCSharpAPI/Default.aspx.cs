4a6ed5767c6bac56a80a983cd20f4edd5584ae45
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2017-01-25 13:49:29 -0200

    Primeira Versão
    Exemplo de uso da API mixpanel-csharp

---
+++ MixPanelCSharpAPI/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using Mixpanel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MixPanelCSharpAPI {
+    //PM> Install-Package mixpanel-csharp
+    //https://www.nuget.org/packages/mixpanel-csharp/
+
+    // EXEMPLO 01:
+    //   mixpanel.track("Assinatura", {
+    //       "tpPagamento": "Boleto",
+    //       "idPlano": "01"
+    //   });
+
+    // EXEMPLO 02:
+    //   mixpanel.track("Assinatura", {
+    //       "tpPagamento": "Cartão",
+    //       "idPlano": "01"
+    //   });
+
+    //Outros links:
+    //Guide: https://mixpanel.com/help/reference
+    //https://github.com/eealeivan/mixpanel-csharp
+    //https://github.com/chrisnicola/Mixpanel.NET
+
+    public partial class _Default : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            
+        }
+
+        //LEMBRAR DE ADCIONAR <%@ Page Async="true" %> na pagina ASPX
+        protected async void Button1_Click(object sender, EventArgs e) {
+            var mc = new MixpanelClient("[API SECRET]");
+            await mc.TrackAsync("Assinatura", new {
+                idPlano = "12345",
+                dtPagamento = DateTime.Now,
+                tpPagamento = "Boleto"
+            });
+        }
+
+        //RESULT JSON:
+        //{
+        //  "event": "Assinatura",
+        //  "properties": {
+        //    "token": "e3bc4100330c35722740fb8c6f5abddc",
+        //    "idPlano": "12345",
+        //    "dtPagamento": "2015/06/06 00:00:00",
+        //    "tpPagamento": "Boleto"
+        //  }
+        //}
+    }
+}
---
+++ MixPanelCSharpAPI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MixPanelCSharpAPI.Startup))]
+namespace MixPanelCSharpAPI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
